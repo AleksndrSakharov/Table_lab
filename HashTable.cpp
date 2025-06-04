69c056ee2b4fc42541556c8ec437d13114c05a0b
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2025-06-04 17:04:01 +0300

    213

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,10 +1,11 @@
 #include "HashTable.h"
 
 size_t HashTable::HashFunc(const Key key){
-    size_t HashValue = 0;
+    size_t HashValue = 0x811c9dc5;
     size_t size = key.size();
-    for (size_t i = 0; i < size; i++){
-        HashValue += static_cast<size_t>(key[i]); 
+    for (char b : key){
+        HashValue = (HashValue * 0x01000193) & 0xFFFFFFFF;
+        HashValue ^= b;
     }
     return HashValue;
 }
